dcdbb77c9a4a04237628397cfd41b937c1623ea8
Ruthger Dijt <ruthgerdijt@gmail.com>
2020-11-26 22:06:09 +0100

    fix

--- src/gdlibrary.cpp
+++ src/gdlibrary.cpp
@@ -1,19 +1,21 @@
 #include "gdnative/EmulGlue.hxx"
 #include "gdnative/components/AnalogRayCast.hxx"
 
+using namespace godot;
+
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
-    godot::Godot::gdnative_init(o);
+    Godot::gdnative_init(o);
 }
 
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
-    godot::Godot::gdnative_terminate(o);
+    Godot::gdnative_terminate(o);
 }
+
+template<class ...T>
+void register_classes() { (register_class<T>(), ...); };
 
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
     godot::Godot::nativescript_init(handle);
-    godot::register_class<godot::EmulGlue>();
-    godot::register_class<godot::AnalogRayCast>();
-    godot::register_class<godot::CustomIter>();
-
+    register_classes<EmulGlue, AnalogRayCast>();
 }
 
